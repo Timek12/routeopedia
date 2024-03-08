b2f298d1b45b713599ca15812d9a83ceb8bf016d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-08 10:54:56 +0100

    Add default rout to home rout and append header component to each route component

--- src/Header.jsx
+++ src/Header.jsx
@@ -25,13 +25,13 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link" href="/about">
+                  About
                 </a>
               </li>
               <li className="nav-item dropdown">
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./About";
 import NotFound from "./NotFound";
 import CryptoDetail from "./CryptoDetail";
+import Home from "./Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Header />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/About" element={<About />}></Route>
         <Route path="/NotFound" element={<NotFound />}></Route>
         <Route path="/CryptoDetail" element={<CryptoDetail />}></Route>
